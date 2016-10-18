7e40190bf8025d43b5393ddccf5b0d7333a4b844
Mitchell Deane <mitchelld20@yahoo.com>
2016-10-18 16:59:31 -0500

    Added search bar functionality with functional get request and git ignored api_key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
-export function selectItem(item) {
-	
+import axios from 'axios';
+import API_KEY from '../api_key';
+
+//api_key is a file that is in the .gitignore. Make a file that exports the api key as a const.
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; 
+//use backticks - ` `- and ${} to add function/variable values to strings
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	// the root url with the request information query (which city, followed by
+	// a default of the country which will always be the US for now until modified)
+	const request = axios.get(url);
+	//axios allows for an easy get request with promises. We will use this 
+	//on the url and make that our payload.
+
 	return {
-		type: 'ITEM_SELECTED',
-		payload: item
+		type: FETCH_WEATHER,
+		payload: request
 	}
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+
+		this.onInputChange = this.onInputChange.bind(this); //always bind onInputChange to this specific version of "this"
+		//now when we call onChange, it will refer to this as in this.state and not in reference to the input
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event){
+		console.log(event.target.value); 
+		this.setState({term: event.target.value});
+		//This updates the this.state's term value to what is being put through it (seen in input)
+	}
+
+	onFormSubmit(event){
+		event.preventDefault();
+
+		this.props.fetchWeather(this.state.term);
+		this.setState({term: ""}); //upon clicking Submit, it will call the action creator with search 
+		//term then rerender the search bar as empty
+
+	}
+
 	render() {
 		return (
-			<form className="input-group">
-				<input />
+			<form className="input-group" onSubmit={this.onFormSubmit}>
+				<input 
+					placeholder="Get a five day forecase in your favorite cities" 
+					className="form-control" 
+					value={this.state.term} 
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className = "btn btn-secondary">Submit</button>
 				</span>
@@ -13,3 +46,10 @@ export default class SearchBar extends Component {
 	}
 
 }
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({fetchWeather}, dispatch);
+}
+//allows for this.props.fetchWeather
+
+export default connect(null, mapDispatchToProps)(SearchBar);
